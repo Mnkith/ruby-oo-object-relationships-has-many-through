367b3e82440e7dad48dc78054ce4daa7ab16a187
Mnkith <munkithabid@gmail.com>
2020-11-13 23:46:41 -0600

    commit tree 993ee00552fb84ae766e357cbee4bd91bceda256

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,25 @@
 class Customer
-  
-end
+
+  attr_accessor :name, :age
+  attr_reader :waiters, :meals
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+    @meals = []
+    @waiters = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, tot, tip)
+    @meals << Meal.new(waiter, self, tot, tip)
+    @waiters << waiter
+    waiter.meals << @meals.last
+  end
+end 
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
 
+  attr_accessor :name, :yrs_experience
+  attr_reader :meals
+  
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+    @meals = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(cust, tot, tip)
+    Meal.new(self, cust, tot, tip)
+  end
+
+  def best_tipper
+    @meals.max{|a, b| a.tip <=> b.tip}.customer
+  end
 end
